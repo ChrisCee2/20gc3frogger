60c1e1e4bdd49314106f0c002a051fada302846f
ChrisCee2 <chrischung411@gmail.com>
2025-07-15 22:23:16 -0400

    GC3-9: Person moves with move controller

--- move_controller.gd
+++ move_controller.gd
@@ -4,7 +4,7 @@ class_name MoveController extends Node
 @onready var move_timer: Timer = $MoveTimer
 
 @export var object: PhysicsBody2D
-@export_range(0, 10) var move_interval: float = 0.05 # In seconds
+@export_range(0, 10) var move_interval: float = 0.0 # In seconds
 
 var animation_interval: float = 0.025
 var move_amount: float = 32
@@ -21,7 +21,7 @@ func can_move() -> bool:
 	return move_timer.time_left == 0
 
 func move(direction: Vector2) -> void:
-	if direction == Vector2.ZERO and can_move():
+	if direction == Vector2.ZERO or not can_move():
 		return
 	object.global_position = desired_position
 	update_position(direction * move_amount)
