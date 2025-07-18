8deca05a22f7924aa44d416e14406cf185c36ea3
ChrisCee2 <chrischung411@gmail.com>
2025-07-17 23:57:32 -0400

    GC3-4: Added soap animation to soap

--- objects/soap.gd
+++ objects/soap.gd
@@ -1,6 +1,7 @@
 class_name Soap extends Area2D
 
 @onready var move_controller: MoveController = $MoveController
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var should_move_left: bool = true
 @export_range(0, 10) var move_interval: float = 0.3
@@ -8,6 +9,7 @@ class_name Soap extends Area2D
 var move_direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
+	_animation_player.play("idle")
 	move_controller.move_interval = move_interval
 	move_direction = Vector2.LEFT if should_move_left else Vector2.RIGHT
 	move_controller.moved.connect(_on_move)
