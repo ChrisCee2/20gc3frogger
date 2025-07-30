497659ee1f0add923c79c415bbdc71725f9f089f
ChrisCee2 <chrischung411@gmail.com>
2025-07-30 17:46:17 -0400

    GC3-5: Add dog sprite

---
+++ objects/dog.gd
@@ -0,0 +1,40 @@
+class_name Dog extends Area2D
+
+@onready var move_controller: MoveController = $MoveController
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var animation_states: Dictionary[int, Array] = {
+	0: ["left", "right"],
+	1: ["left1", "right1"],
+	2: ["left2", "right2"]
+}
+var animation_state_count: int = 3
+var current_animation_state: int = 0
+
+var is_started: bool = false
+
+func _ready() -> void:
+	set_deferred("monitorable", false)
+	current_animation_state = 0
+	animation_player.play(animation_states[current_animation_state][0])
+	move_controller.moved.connect(update_animation_state)
+	hide()
+
+func _process(delta: float) -> void:
+	if is_started:
+		move_controller.update()
+
+func start() -> void:
+	is_started = true
+	set_deferred("monitorable", true)
+	show()
+
+func move(move_direction: Vector2) -> void:
+	move_controller.shift(move_direction)
+
+func update_animation_state(move_offset: Vector2) -> void:
+	current_animation_state = (current_animation_state + 1) % animation_state_count
+	if move_offset.x < 0:
+		animation_player.play(animation_states[current_animation_state][0])
+	else:
+		animation_player.play(animation_states[current_animation_state][1])
