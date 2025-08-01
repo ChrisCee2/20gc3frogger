6454a9954ba2bb120d230ea7c339cbef3035309b
ChrisCee2 <chrischung411@gmail.com>
2025-07-31 23:29:07 -0400

    GC3-7: Add lose from water art

--- ui/end_frame.gd
+++ ui/end_frame.gd
@@ -5,6 +5,7 @@ class_name EndFrame extends TextureRect
 var win_sprite_sheet: CompressedTexture2D = preload("res://sprites/sprite_sheets/win_frame.png")
 var lose_dog_sprite_sheet: CompressedTexture2D = preload("res://sprites/sprite_sheets/lose_dog_frame.png")
 var lose_oob_sprite_sheet: CompressedTexture2D = preload("res://sprites/sprite_sheets/lose_oob_frame.png")
+var lose_water_sprite_sheet: CompressedTexture2D = preload("res://sprites/sprite_sheets/lose_water_frame.png")
 
 func _ready() -> void:
 	visible = false
@@ -21,5 +22,10 @@ func show_lose_dog() -> void:
 
 func show_lose_oob() -> void:
 	texture.atlas = lose_oob_sprite_sheet
+	visible = true
+	animation_player.play("frame")
+
+func show_lose_water() -> void:
+	texture.atlas = lose_water_sprite_sheet
 	visible = true
 	animation_player.play("frame")
